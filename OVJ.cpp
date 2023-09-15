436d91e23d19a2864ce2f278fce2583f10e20316
Jens <106508255+MPRoses@users.noreply.github.com>
2023-09-15 15:27:06 +0200

    Update OVJ.cpp

--- OVJ.cpp
+++ OVJ.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <list>
-#include <set>
 #include <ctime>
 #include <cmath>
 #include <string>
@@ -9,9 +8,11 @@
 using namespace std;
 
 int main() {
+  srand (time(NULL)); // seed creation
+  time_t t = time(NULL); // same thing
 
-  time_t t = time(NULL);
-  struct tm date;
+  // lines voor initialization cTime
+  struct tm date; 
   if (gmtime_r(&t, &date) == NULL) {
     return 1;
   }
@@ -19,11 +20,26 @@ int main() {
   int Geboortejaar;
   int Geboortemaand;
   int Geboortedag;
+
   int Dag = date.tm_mday;
   int Maand = date.tm_mon + 1;
   int Jaar = date.tm_year + 1900;
 
+  int LeeftijdJaren;
+  int LeeftijdMaanden;
+
+  int dagenVoorbijMaand = 0; // moet gezet worden als 0
+  int aantalSchrikkeljarenVoorbij;
+  int aantalDagenVoorbijVanDeJaren;
+  int aantalDagenVoorbijMaandEnDagen;
+  int aantalDagenVoorbijTotaal;
+
   bool isSchrikkelJaar = (date.tm_year % 4 == 0);
+
+  int dagenInMaand[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+  string dagen[] = {"zo", "m", "d", "w", "d", "v", "za"};
+  string gokDag;
 
   cout << "Geef uw geboortejaar: ";
   cin >> Geboortejaar;
@@ -58,10 +74,6 @@ int main() {
     }
   }
 
-  set<int> Maand31 {1, 3, 5, 7, 8, 10, 12};
-  set<int> Maand30 {4, 6, 9, 11};
-  set<int> Maand28 {2};
-
   cout << "Geef uw geboortedag: ";
   cin >> Geboortedag;
 
@@ -69,37 +81,18 @@ int main() {
     cout << "Gefeliciteerd!" << endl; 
   }
 
-  if (Maand31.find(Maand) != Maand31.end()) {
-    if (Geboortedag > 31 || Geboortedag <= 0) {
+  if (Maand == 2 && isSchrikkelJaar) {
+    if (Geboortedag > 29 || Geboortedag <= 0) {
       cout << "Ongeldige Geboortedag" << endl;
       return 1;
     }
+  } else if (Geboortedag > dagenInMaand[Geboortemaand] || Geboortedag <= 0) {
+    cout << "Ongeldige Geboortedag" << endl;
+    return 1;
   }
 
-   if (Maand30.find(Maand) != Maand30.end()) {
-     if (Geboortedag > 30 || Geboortedag <= 0) {
-      cout << "Ongeldige Geboortedag" << endl;
-      return 1;
-    }
-  }
-  if (Maand28.find(Maand) != Maand28.end()) {
-    if (isSchrikkelJaar) {
-      if (Geboortedag > 29 || Geboortedag <= 0) {
-        cout << "Ongeldige Geboortedag" << endl;
-        return 1;
-      }
-    } else {
-      if (Geboortedag > 28 || Geboortedag <= 0) {
-        cout << "Ongeldige Geboortedag" << endl;
-        return 1;
-      }
-    }
-   
-  }
-
-  int LeeftijdJaren = Jaar - Geboortejaar;
-
-  int LeeftijdMaanden = (Maand - Geboortemaand + 12) % 12;
+  LeeftijdJaren = Jaar - Geboortejaar;
+  LeeftijdMaanden = (Maand - Geboortemaand + 12) % 12;
 
   if ((Geboortejaar == (Jaar - 10) || Geboortejaar == (Jaar - 101)) &&
       Geboortemaand == Maand) {
@@ -124,42 +117,49 @@ int main() {
     LeeftijdMaanden = (LeeftijdMaanden + 12) % 12;
   }
 
-  cout << "Je bent " << LeeftijdJaren << " jaar en " << LeeftijdMaanden << " maanden oud." << endl;
-
-  int dagenInMaand[] = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-
-  int dagenVoorbijMaand = 0;
+  cout << "Je bent " << LeeftijdJaren << " jaar en " << LeeftijdMaanden << " maanden oud" << endl;
 
   for (int x = 1; x < Geboortemaand; x++) {
     dagenVoorbijMaand += dagenInMaand[x];
   }
 
+  aantalSchrikkeljarenVoorbij = (Geboortejaar - 1900) / 4;
+  aantalDagenVoorbijVanDeJaren = (Geboortejaar - 1900) * 365 + aantalSchrikkeljarenVoorbij;
+  aantalDagenVoorbijMaandEnDagen = dagenVoorbijMaand + Geboortedag;
+  cout << aantalDagenVoorbijMaandEnDagen << endl;
 
-  int AantalSchrikkeljarenVoorbij = (Geboortejaar - 1900) / 4;
-  int aantalDagenVoorbijVanDeJaren = (Geboortejaar - 1900) * 365 + AantalSchrikkeljarenVoorbij;
-  int aantalDagenVoorbijMaandEnDagen = dagenVoorbijMaand + Geboortedag;
+  aantalDagenVoorbijTotaal = aantalDagenVoorbijMaandEnDagen + aantalDagenVoorbijVanDeJaren;
 
-  int aantalDagenVoorbijTotaal = aantalDagenVoorbijMaandEnDagen + aantalDagenVoorbijVanDeJaren;
+  cout << aantalDagenVoorbijTotaal << endl;
 
   double dagGeboren = fmod(aantalDagenVoorbijTotaal, 7);
 
-  string dagen[] = {"zo", "m", "d", "w", "d", "v", "za"};
 
-  string gokDag;
   cout << "Op welke dag ben je geboren? Geef in kleine letters de eerste letter van de dag, en ook de tweede als het om za of zo gaat" << endl;
   cin >> gokDag;
 
 
   if (gokDag == dagen[static_cast<int>(round(dagGeboren))]) {
-    cout << "Goed geraden haha!"<< endl;
-
+    cout << "Goed geraden!"<< endl;
   } else {
-    cout << "Fout geraden!"
+    cout << "Fout geraden!" << endl;
     return 1;
   }
 
+  // nu tijd voor sectie 2: het stellen van vragen.
 
-  // nu tijd voor sectie 2: het stellen van vragen.*/
+ 
+  int getal1 = (rand() % 89) + 10; 
+  int getal2 = (rand() % 89) + 10; 
+  int deelGetal1 = (getal2 % 10) * getal1;
+  int deelGetal2 = (getal2 / 10) * 10 * getal1;
+  int antwoord = getal1*getal2;
+  
+  // if deelGetal1
+  cout << " " << getal1 << endl << " " << getal2 << " x" << endl; 
+  cout << " --" << endl << "" << deelGetal1 << endl << deelGetal2 << " +" << endl;
+  cout << " ---" << endl << antwoord << endl;
+  
 
   return 0;
 
