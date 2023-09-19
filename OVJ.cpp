7e45eee79480bfad904b2268f8b237b1f338761a
Jens <106508255+MPRoses@users.noreply.github.com>
2023-09-19 12:26:31 +0200

    Update OVJ.cpp

--- OVJ.cpp
+++ OVJ.cpp
@@ -117,7 +117,7 @@ int main() {
     LeeftijdMaanden = (LeeftijdMaanden + 12) % 12;
   }
 
-  cout << "Je bent " << LeeftijdJaren << " jaar en " << LeeftijdMaanden << " maanden oud" << endl;
+  cout << endl << "Je bent " << LeeftijdJaren << " jaar en " << LeeftijdMaanden << " maanden oud" << endl;
 
   for (int x = 1; x < Geboortemaand; x++) {
     dagenVoorbijMaand += dagenInMaand[x];
@@ -126,13 +126,10 @@ int main() {
   aantalSchrikkeljarenVoorbij = (Geboortejaar - 1900) / 4;
   aantalDagenVoorbijVanDeJaren = (Geboortejaar - 1900) * 365 + aantalSchrikkeljarenVoorbij;
   aantalDagenVoorbijMaandEnDagen = dagenVoorbijMaand + Geboortedag;
-  cout << aantalDagenVoorbijMaandEnDagen << endl;
 
   aantalDagenVoorbijTotaal = aantalDagenVoorbijMaandEnDagen + aantalDagenVoorbijVanDeJaren;
 
-  cout << aantalDagenVoorbijTotaal << endl;
-
-  double dagGeboren = fmod(aantalDagenVoorbijTotaal, 7);
+  double dagGeboren = aantalDagenVoorbijTotaal % 7;
 
 
   cout << "Op welke dag ben je geboren? Geef in kleine letters de eerste letter van de dag, en ook de tweede als het om za of zo gaat" << endl;
@@ -140,25 +137,92 @@ int main() {
 
 
   if (gokDag == dagen[static_cast<int>(round(dagGeboren))]) {
-    cout << "Goed geraden!"<< endl;
+    cout << "Goed geraden!"<< endl << endl;
   } else {
     cout << "Fout geraden!" << endl;
     return 1;
   }
 
-  // nu tijd voor sectie 2: het stellen van vragen.
-
+  // vanaf hier asl gebruiker 30 jaar of ouder is moet netter aangesproken worden
  
   int getal1 = (rand() % 89) + 10; 
   int getal2 = (rand() % 89) + 10; 
   int deelGetal1 = (getal2 % 10) * getal1;
   int deelGetal2 = (getal2 / 10) * 10 * getal1;
   int antwoord = getal1*getal2;
+  int getalGok = 0;
+  double epsilon = 0.1;
   
-  // if deelGetal1
-  cout << " " << getal1 << endl << " " << getal2 << " x" << endl; 
-  cout << " --" << endl << "" << deelGetal1 << endl << deelGetal2 << " +" << endl;
-  cout << " ---" << endl << antwoord << endl;
+  cout << getal1 << endl << getal2 << " x" << endl << "--" << endl; 
+
+  cout << "Wat is het antwoord van de bovenstaande berekening?" << endl;
+  cout << "Gok het binnen een foutmarge van " << (epsilon * 100) << "%!" << endl;
+  cin >> getalGok;
+
+  cout << endl << "Het antwoord was:" << endl;
+
+
+  cout << getal1 << endl << getal2 << " x" << endl; 
+
+  if (deelGetal1 == 0 || deelGetal2 == 0) {
+    cout << "---" << endl << antwoord << endl;
+  } else {
+    cout << "--" << endl << deelGetal1 << endl << deelGetal2 << " +" << endl;
+    cout << "---" << endl << antwoord << endl;
+  }
+
+  string meerkeuzeGok;
+
+  if ((getalGok >= antwoord * (1 - epsilon)) && (getalGok <= antwoord * (1 + epsilon))) {
+    cout << endl << "Binnen de juiste foutmarges gegokt!" << endl;
+    cout << "Je bent ofwel geschikt voor een exacte studie, gefeliciteerd!" << endl;
+  } else {
+    cout << "Buiten de juiste foutmarges gegokt!" << endl << endl;
+
+    cout << "Misschien is een alpha-studie meer geschikt?" << endl;
+    cout << "Beantwoord de volgende kunstvraag:" << endl;
+    cout << "*Geef de letter van het juiste antwoord!" << endl << endl;
+
+    if (LeeftijdJaren >= 30) {
+      cout << "Welke kunststroming werd tussen 1909 en 1914 veel beoefend en staat beroemde schilder Pablo Picasso bekent om?" << endl;
+      cout << "A) Kubisme" << endl;
+      cout << "B) Pointilisme" << endl;
+      cout << "C) Realisme" << endl;
+      cout << "D) Abstract Expressionisme" << endl;
+
+      cin >> meerkeuzeGok;
+
+      if (meerkeuzeGok == "A" || meerkeuzeGok == "a") {
+        cout << endl << "Juist geraden! Het kubisme is te herkennen aan: geometrische vormen, veelal felle kleuren en een collagestijl." << endl;
+        cout << "Je bent ofwel geschikt voor een alpha-studie, gefeliciteerd!" << endl;
+      } else {
+        cout << endl << "Fout geraden!" << endl;
+        cout << "Het juiste antwoord was A: het Kubisme" << endl;
+        return 1;
+      }
+    } else {
+      cout << "Welke bekende straatartiest had een versnipperaar ingebouwd in een van zijn schilderijen?" << endl;
+      cout << "A) Banksy" << endl;
+      cout << "B) Tristan Eaton" << endl;
+      cout << "C) Mark Jenkins" << endl;
+      cout << "D) Blu" << endl;
+
+      cin >> meerkeuzeGok;
+
+      if (meerkeuzeGok == "A" || meerkeuzeGok == "a") {
+        cout << endl << "Juist geraden! Vanwege deze stunt steeg de prijs van het schilderij met 24 miljoen euro!" << endl;
+        cout << "Je bent ofwel geschikt voor een alpha-studie, gefeliciteerd!" << endl;
+      } else {
+        cout << endl << "Fout geraden!" << endl;
+        cout << "Het juiste antwoord was A: Banksy" << endl;
+        return 1;
+      }
+    }
+
+
+  }
+
+
   
 
   return 0;
